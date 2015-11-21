2878b3216bae0b46595fcd5b35424025c6e4a42b
Kkmonster <kkmonster@hotmail.com>
2015-11-22 00:13:02 +0700

    update

--- drone_rx/drone_rx.ino
+++ drone_rx/drone_rx.ino
@@ -6,7 +6,7 @@
 #include <ESP8266WiFi.h>
 #include <EEPROM.h>
 #include <MicroGear.h>
-
+#include "oled.h"
 
 
 const char* ssid     = "NAT.WRTNODE";
@@ -17,12 +17,16 @@ const char* password = "devicenetwork";
 #define GEARSECRET  "tuVDXPN8QJYRtCQzrA8yeQwhxu8fUA"
 #define SCOPE       "rw:/drone"
 
+uint8_t connect_state = 0;
+unsigned long time_now = 0;
+unsigned long time_prev_netpie = 0;
+unsigned long time_prev_sensors = 0;
 int rx_buffer[4] = {0};
-
+float yaw, pitch, roll, throttle;
 WiFiClient client;
 AuthClient *authclient;
 
-int timer = 0;
+uint timer = 0;
 MicroGear microgear(client);
 
 void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen) 
@@ -44,94 +48,193 @@ void onMsghandler(char *topic, uint8_t* msg, unsigned int msglen)
     }
   }
 
-  msg[msglen] = '\0';
-  Serial.println((char *)msg);
-  Serial.print("   ");
-  Serial.print(rx_buffer[0]);
-  Serial.print(rx_buffer[1]);
-  Serial.print(rx_buffer[2]);
-  Serial.println(rx_buffer[3]);
+  // throttle = rx_buffer[0];
+  // yaw = rx_buffer[1];
+  // pitch = rx_buffer[2];
+  // roll = rx_buffer[3];
+
+  // msg[msglen] = '\0';
+  // Serial.println((char *)msg);
+  // Serial.print("   ");
+  Serial.write(rx_buffer[3]);
+  Serial.write(rx_buffer[2]);
+  Serial.write(rx_buffer[0]);
+  Serial.write(rx_buffer[1]);
+  Serial.write(0xfe);
+  digitalWrite(16, !digitalRead(16));
+
 }
 
 void onFoundgear(char *attribute, uint8_t* msg, unsigned int msglen) {
-  Serial.print("Found new member --> ");
-  for (int i = 0; i < msglen; i++)
-    Serial.print((char)msg[i]);
-  Serial.println();
+  // Serial.print("Found new member --> ");
+  // for (int i = 0; i < msglen; i++)
+  //   Serial.print((char)msg[i]);
+  // Serial.println();
 }
 
 void onLostgear(char *attribute, uint8_t* msg, unsigned int msglen) {
-  Serial.print("Lost member --> ");
-  for (int i = 0; i < msglen; i++)
-    Serial.print((char)msg[i]);
-  Serial.println();
+  // Serial.print("Lost member --> ");
+  // for (int i = 0; i < msglen; i++)
+  // Serial.print((char)msg[i]);
+  // Serial.println();
 }
 
 void onConnected(char *attribute, uint8_t* msg, unsigned int msglen) {
-  Serial.println("Connected to NETPIE...");
+  //Serial.println("Connected to NETPIE...");
   microgear.setName("RX");
   microgear.subscribe("/drone");
+  display.clearDisplay();
+  display.setCursor(0,0); 
+  display.println("Connected...");
+  display.display(); 
+  connect_state = 1;
 }
 
 
 void setup() {
   /* Event listener */
+  pinMode(16,OUTPUT);
+  digitalWrite(16, 1);
   microgear.on(MESSAGE, onMsghandler);
   microgear.on(PRESENT, onFoundgear);
   microgear.on(ABSENT, onLostgear);
   microgear.on(CONNECTED, onConnected);
 
   Serial.begin(115200);
+  setup_oled();
+
+  display.setCursor(0,0); 
+  display.println("CMMC.Espresso.NETPIE");
+  display.display();
+  delay(2000);
+
+  display.print("Starting...");
+  display.display();
+  delay(500);
+  display.println(" OK!");
+  display.display();
+  delay(100);
 
 
 
-  Serial.println("Starting...");
 
+  if (WiFi.begin(ssid, password)) 
+  {
 
+    display.print("WIFI");
+    display.display(); 
 
-
-  if (WiFi.begin(ssid, password)) {
-
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
       delay(500);
-      Serial.print(".");
+      display.print(".");
+      display.display(); 
     }
 
-    Serial.println("WiFi connected");
-    Serial.println("IP address: ");
-    Serial.println(WiFi.localIP());
+    display.println("OK!");
+    display.print("IP : ");
+    display.println(WiFi.localIP());
+    display.display(); 
 
     //uncomment the line below if you want to reset token -->
     //microgear.resetToken();
     microgear.init(GEARKEY, GEARSECRET, SCOPE);
     microgear.connect(APPID);
   }
+
+  display.clearDisplay();
+  display.setCursor(0,0); 
+  display.println("");
+  display.print(" MicroGear Connecting.");
+  display.display();
+
+  time_now = millis();
+  time_prev_netpie = time_now;
 }
 
 void loop() 
 {
+  time_now = millis();
 
 
+  display.print(".");
+  display.display();
 
-  if (microgear.connected()) {
-    //Serial.println("connected");
-    microgear.loop();
-    if (timer >= 1000) {
-      //      Serial.println("Publish...");
-      //      microgear.publish("/drone", "publish_hello_too");
+  if (connect_state)
+  {
+    display.clearDisplay();
+    display.setCursor(0,0); 
+    display.display();
 
-      //      microgear.chat("kk_0x", "Hello");
-      timer = 0;
+    time_now = millis();
+    time_prev_netpie = time_now;
+    time_prev_sensors = time_now;
+
+
+    while(1)
+    {
+      delay(1);
+      time_now = millis();
+
+      microgear.loop();
+
+      float dt = time_now - time_prev_sensors;
+      if(dt >= 10)                                 //  lower than 100 Hz
+      {   
+        time_prev_sensors = time_now;
+
+
+      }
+
+      if(time_now - time_prev_netpie >= 1000)     // 10Hz
+      {
+        time_prev_netpie = time_now;
+
+        display.clearDisplay();
+        display.setCursor(0,0); 
+        display.println("CMMC.Espresso.NETPIE");
+        display.println(rx_buffer[0]);
+        display.println(rx_buffer[1]);
+        display.println(rx_buffer[2]);
+        display.println(rx_buffer[3]);
+        display.display(); 
+
+      }
     }
-    else timer += 100;
   }
-  else {
-    Serial.println("connection lost, reconnect...");
-    if (timer >= 5000) {
-      microgear.connect(APPID);
-      timer = 0;
-    }
-    else timer += 100;
-  }
-  delay(100);
+
+  if(time_now - time_prev_netpie > 5000)
+  {
+    microgear.connect(APPID);
+    time_prev_netpie = time_now;
+  } 
+  delay(500);
+
+  // if (microgear.connected()) {
+  //   //Serial.println("connected");
+  //   microgear.loop();
+  //   if (timer >= 1000) {
+  //     //      Serial.println("Publish...");
+  //     //      microgear.publish("/drone", "publish_hello_too");
+
+  //     //      microgear.chat("kk_0x", "Hello");
+  //     timer = 0;
+  //   }
+  //   else timer += 1;
+  // }
+  // else 
+  // {
+  //   display.clearDisplay();
+  //   display.setCursor(0,0); 
+  //   display.println("connection lost, reconnect...");
+  //   display.display(); 
+
+  //   if (timer >= 5000) 
+  //   {
+  //     microgear.connect(APPID);
+  //     timer = 0;
+  //   }
+  //   else timer += 1;
+  // }
+  // delay(1);
 }
---
+++ drone_rx/oled.h
@@ -0,0 +1,45 @@
+/*********************************************************************
+This is an example for our Monochrome OLEDs based on SSD1306 drivers
+
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/category/63_98
+
+This example is for a 128x64 size display using I2C to communicate
+3 pins are required to interface (2 I2C and one reset)
+
+Adafruit invests time and resources providing this open source code, 
+please support Adafruit and open-source hardware by purchasing 
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.  
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
+
+//#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <ESP_Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+// #if (SSD1306_LCDHEIGHT != 64)
+// #error("Height incorrect, please fix Adafruit_SSD1306.h!");
+// #endif
+
+void setup_oled()   
+{                
+
+  // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+  // init done
+
+  // Clear the buffer.
+  display.clearDisplay();
+  display.display();
+  // text display tests
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+
+}
